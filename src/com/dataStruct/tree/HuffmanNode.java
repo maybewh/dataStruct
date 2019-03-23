75380da9c1a72e86c9a9ebd8a0e70945bcb82b62
maybewh <122131004@qq.com>
2019-03-23 19:22:23 +0800

    huffman tree

---
+++ src/com/dataStruct/tree/HuffmanNode.java
@@ -0,0 +1,10 @@
+package com.dataStruct.tree;
+
+public class HuffmanNode {
+	
+	public int data;
+	public char c;
+	
+	public HuffmanNode leftNode;
+	public HuffmanNode rightNode;
+}
--- src/com/dataStruct/tree/MyComparator.java
+++ src/com/dataStruct/tree/MyComparator.java
@@ -2,13 +2,13 @@ package com.dataStruct.tree;
 
 import java.util.Comparator;
 
-public class MyComparator implements Comparator<HuffmanTree>{
+public class MyComparator implements Comparator<HuffmanNode>{
 	
 	/**
 	 * ÕıĞòÅÅÁĞ
 	 */
 	@Override
-	public int compare(HuffmanTree x, HuffmanTree y) {
+	public int compare(HuffmanNode x, HuffmanNode y) {
 		// TODO Auto-generated method stub
 		return x.data - y.data;
 	}
